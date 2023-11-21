c9d7ae598e877b78d949356939d94b600024a051
FORNOFF Leo <fornoff1u@tncy-1-18-04.telecomnancy.site.univ-lorraine.fr>
2023-11-21 11:06:33 +0100

    Game configuration

--- MOCI/src/main/java/RPG/GameConfiguration/GameConfiguration.java
+++ MOCI/src/main/java/RPG/GameConfiguration/GameConfiguration.java
@@ -1,6 +1,47 @@
-package main.java.RPG;
+package main.java.RPG.GameConfiguration;
+
 
 public class GameConfiguration {
-    
 
+    private static GameConfiguration instance;
+    private GameDifficulty difficulty;
+    private int teamSize;
+
+
+    public static GameConfiguration getInstance(int difficultyLevel, int teamSize) {
+        if (instance==null) return instance;
+        return new GameConfiguration(difficultyLevel, teamSize);
+    }
+
+
+    private GameConfiguration(int difficultyLevel, int teamSize) {
+        this.difficulty = new GameDifficulty(difficultyLevel);
+        setTeamSize(teamSize);
+    }
+
+
+    public void setTeamSize(int teamSize) {
+        if (teamSize < 1) teamSize = 1;
+        this.teamSize = teamSize;
+    }
+
+    public void setDifficultyLevel(int difficulty) {
+        this.difficulty.setDifficultyLevel(difficulty);
+    }
+
+    publci void setDifficulty(GameDifficulty gameDifficulty) {
+        this.difficulty = gameDifficulty;
+    }
+
+    public int getTeamSize() {
+        return this.teamSize;
+    }
+
+    public int getDifficultyLevel() {
+        return this.difficulty.getDifficultyLevel();
+    }
+
+    public GameDifficulty getDifficulty() {
+        return this.difficulty;
+    }
 }
--- MOCI/src/main/java/RPG/GameConfiguration/GameDifficulty.java
+++ MOCI/src/main/java/RPG/GameConfiguration/GameDifficulty.java
@@ -1,2 +1,22 @@
-package main.java.RPG.GameConfiguration;public class GameDifficulty {
+package main.java.RPG.GameConfiguration;
+
+public class GameDifficulty {
+    // To set the difficulty of a game
+    private int difficultyLevel;
+    private static final int minDifficulty = 0;
+    private static final int maxDifficulty = 5;
+
+    public GameDifficulty(int difficultyLevel) {
+        if (difficultyLevel < minDifficulty) difficultyLevel = minDifficulty;
+        if (difficultyLevel > maxDifficulty) difficultyLevel = maxDifficulty;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public int getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+    protected void setDifficultyLevel(int difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
 }
